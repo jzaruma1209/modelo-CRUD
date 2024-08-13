2fb1a1210e06bf5422084551fe41b68f627602cd
Idarteaga <173113463+LastVizard405@users.noreply.github.com>
2024-08-13 15:32:48 -0500

    crud-p4

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import useCrud from './hooks/useCrud';
+import FormUser from './components/FormUser';
+import UserCard from './components/UserCard';
+
+function App() {
+	const [userSelected, setUserSelected] = useState();
+	const [formIsOpen, setFormIsOpen] = useState(false);
+	const [users, getUsers, createUser, deleteUser, updateUser] = useCrud('/users/');
+	useEffect(() => {
+		getUsers();
+	}, []);
+
+	const handleOpenForm = () => {
+		setFormIsOpen(true);
+	};
+
+	return (
+		<div className="app">
+			<header className="user__header">
+				<h1>Funny Heroes Users</h1>
+				<button onClick={handleOpenForm} className="new__user-btn">
+					New User +
+				</button>
+			</header>
+
+			<FormUser
+				createUser={createUser}
+				userSelected={userSelected}
+				setUserSelected={setUserSelected}
+				updateUser={updateUser}
+				setFormIsOpen={setFormIsOpen}
+				formIsOpen={formIsOpen}
+			/>
+			<section className="user__container flex-container">
+				{users?.map((user) => (
+					<UserCard key={user.id} user={user} deleteUser={deleteUser} setUserSelected={setUserSelected} setFormIsOpen={setFormIsOpen} />
+				))}
+			</section>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,58 @@
+import Swal from 'sweetalert2';
+import './styles/UserCard.css';
+const UserCard = ({ user, deleteUser, setUserSelected, setFormIsOpen }) => {
+	const handleDelete = () => {
+		Swal.fire({
+			title: 'Are you sure?',
+			text: "You won't be able to revert this!",
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#ea4959',
+			cancelButtonColor: '#343434',
+			confirmButtonText: 'Yes, delete it!',
+		}).then((result) => {
+			if (result.isConfirmed) {
+				deleteUser(user.id);
+				Swal.fire({
+					title: 'User Deleted!',
+					text: `User ${user.first_name} ${user.last_name} has been deleted.`,
+					icon: 'success',
+				});
+			}
+		});
+	};
+	const handleEdit = () => {
+		setUserSelected(user);
+		setFormIsOpen(true);
+	};
+
+	return (
+		<article className="user">
+			<img className="user__img" src={user.image_url} alt={`${user.first_name} ${user.last_name}`} />
+			<h3 className="user__name">
+				{user.first_name} {user.last_name}
+			</h3>
+			<hr className="user__hr" />
+			<ul className="user__list grid-container">
+				<li className="user__item grid-container">
+					<span className="user__label">Email:</span>
+					<span className="user__value">{user.email}</span>
+				</li>
+				<li className="user__item grid-container">
+					<span className="user__label">Birthdate:</span>
+					<span className="user__value">{user.birthday}</span>
+				</li>
+			</ul>
+			<footer className="user__footer flex-container">
+				<button className="user__btn user__delete" onClick={handleDelete}>
+					Delete
+				</button>
+				<button className="user__btn user__edit" onClick={handleEdit}>
+					Edit
+				</button>
+			</footer>
+		</article>
+	);
+};
+
+export default UserCard;
